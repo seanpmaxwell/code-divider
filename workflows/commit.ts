f0ec2aa4244341317f0eb52080243479bad8b20d
seanpmaxwell <spmaxwell7@gmail.com>
2026-08-09 22:38:40 -0700

    "Normal development commit"

--- workflows/commit.ts
+++ workflows/commit.ts
@@ -2,8 +2,12 @@ import { $ } from 'execa';
 
 const COMMIT_MESSAGE = 'Normal development commit';
 
-
-await $`git add -A`;
-await $`git commit -m "${COMMIT_MESSAGE}"`;
-
-await $`git push`;
+try {
+  await $`git add -A`;
+  await $`git commit -m "${COMMIT_MESSAGE}"`;
+  await $`git push`;
+} catch (err) {
+  // eslint-disable-next-line no-console
+  console.error(err);
+  process.exit(1);
+}
