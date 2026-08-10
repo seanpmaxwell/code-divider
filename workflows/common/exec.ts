082e5e9b676165c593c8d8c25627b7cf196119aa
Sean Maxwell <10490175+seanpmaxwell@users.noreply.github.com>
2026-08-10 12:30:37 -0700

    Update exec.ts

--- workflows/common/exec.ts
+++ workflows/common/exec.ts
@@ -2,19 +2,19 @@
 import { $ as $$ } from 'execa';
 
 
-let first = true;
+const tracker: Record<string, { first: boolean }> = {};
 
 /**
  * Print execa commands
  */
 const $ = $$({
-  verbose: (verboseLine: string, { type }: { type: string }) => {
-    if (type === 'command') {
-      if (!first) console.log();
-      first = false;
-    }
-    console.log(verboseLine);
-  },
+    verbose: (verboseLine: string, { type, commandId }) => {
+        if (type === 'command') {
+            if (!tracker[commandId].first) console.log();
+            tracker[commandId].first = false;
+        }
+        console.log(verboseLine);
+    },
 });
 
 export default $;
